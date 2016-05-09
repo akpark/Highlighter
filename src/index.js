c06660c9ec3e39b8ca87340bbe75774c1d9e99d5
Andrew Park <ak.park93@gmail.com>
2016-05-09 11:03:29 -0400

    Add routes to index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import reducers from './reducers';
 import App from './components/app';
+import routes from './routes';
+import { Router, browserHistory } from 'react-router';
 
 const createStoreWithMiddleWare = applyMiddleware(
   promise
@@ -12,7 +14,7 @@ const createStoreWithMiddleWare = applyMiddleware(
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleWare(reducers)}>
-    <App />
+    <Router browserHistory={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container')
 );
